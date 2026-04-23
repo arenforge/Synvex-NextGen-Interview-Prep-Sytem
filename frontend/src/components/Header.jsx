943f992beb9dd17a4130b377d8e13dac92787c37
JAMAL RAZA <asjamalraza@gmail.com>
2026-04-23 20:40:41 +0530

    Bug fixes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,12 @@
-function Header(){
-    return <>
-    <h1>Welcome to Synvex</h1>
-    <h2>Next-Gen Interview Prep System</h2>
-    </>
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="site-header">
+            <span className="header-logo">Synvex</span>
+            <span className="header-tagline">Next-Gen Interview Prep</span>
+        </header>
+    );
 }
+
 export default Header;
